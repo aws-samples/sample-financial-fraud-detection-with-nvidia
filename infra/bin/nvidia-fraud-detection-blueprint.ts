95591edc3f75507d4fefbe88e7a4a59bf9b8f291
Elamaran (Ela) Shanmugam <51791117+elamaran11@users.noreply.github.com>
2025-09-15 17:09:31 -0400

    Update nvidia-fraud-detection-blueprint.ts

--- infra/bin/nvidia-fraud-detection-blueprint.ts
+++ infra/bin/nvidia-fraud-detection-blueprint.ts
@@ -9,7 +9,7 @@ const app = new cdk.App();
 // Add CDK Nag checks
 cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
 
-const modelBucketName = "ml-on-containers-gfs"
+const modelBucketName = "ml-on-containers"
 new NvidiaFraudDetectionBlueprint(app, 'NvidiaFraudDetectionBlueprint', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1' },
   modelBucketName: modelBucketName + "-model-registry",
