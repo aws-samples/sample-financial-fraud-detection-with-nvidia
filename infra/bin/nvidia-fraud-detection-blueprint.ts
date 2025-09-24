610b915dbc1913b01ba538a4cd04aee832e364e6
Shardul Vaidya <vshardul@amazon.com>
2025-09-24 16:01:40 -0400

    fix: improvements to documentation

--- infra/bin/nvidia-fraud-detection-blueprint.ts
+++ infra/bin/nvidia-fraud-detection-blueprint.ts
@@ -3,24 +3,39 @@ import * as cdk from 'aws-cdk-lib';
 import { AwsSolutionsChecks } from 'cdk-nag';
 import { NvidiaFraudDetectionBlueprint } from '../lib/nvidia-fraud-detection-blueprint';
 import { TarExtractorStack } from '../lib/tar-extractor-stack';
+import { SageMakerExecutionRoleStack } from '../lib/sagemaker-training-role';
+import { BlueprintECRStack } from '../lib/training-image-repo';
 
 const app = new cdk.App();
 
 // Add CDK Nag checks
 cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
 
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION
+}
+
 const modelBucketName = "ml-on-containers-" + process.env.CDK_DEFAULT_ACCOUNT;
 
 const tarExtractorStack = new TarExtractorStack(app, 'NvidiaFraudDetectionBlueprintModelExtractor', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1' },
+  env: env,
   modelBucketName: modelBucketName,
   synthesizer: new cdk.DefaultStackSynthesizer({
     qualifier: 'nvidia', // Must match the qualifier used in bootstrap
   }),
 });
 
+const sagemakerExecutionRole = new SageMakerExecutionRoleStack(app, 'NvidiaFraudDetectionTrainingRole', {
+  env: env,
+  modelBucketArn: "arn:aws:s3:::" + modelBucketName
+});
+
+const trainingImageRepo = new BlueprintECRStack(app, 'NvidiaFraudDetectionTrainingImageRepo', {
+  env: env
+})
+
 const mainStack = new NvidiaFraudDetectionBlueprint(app, 'NvidiaFraudDetectionBlueprint', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1' },
+  env: env,
   modelBucketName: modelBucketName + "-model-registry",
   synthesizer: new cdk.DefaultStackSynthesizer({
     qualifier: 'nvidia', // Must match the qualifier used in bootstrap
